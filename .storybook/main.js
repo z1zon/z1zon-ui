292e94c201dd002cac610a980861325427b46f1c
hoo00nn <oh.jihoon@linecorp.com>
2021-09-14 15:48:17 +0900

    add storybook alias setting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,11 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
+  },
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
