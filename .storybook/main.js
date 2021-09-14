009133947d0ca658c3e55edc8a0ba18ed734c555
hoo00nn <oh.jihoon@linecorp.com>
2021-09-14 15:42:22 +0900

    add storybook jest plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-controls'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-controls',
+    '@storybook/addon-jest',
+  ],
   typescript: {
     check: false,
     checkOptions: {},
