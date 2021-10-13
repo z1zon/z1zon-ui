59278f387b1fc54d57789c5196b97a71abfcb282
오지훈 <hihiwwg@gmail.com>
2021-10-13 20:50:26 +0900

    Feature/storybook scss (#7)
    * Fix storybook webpack5 compatibility isssue
    
    * setup scss
    
    * remove useless files
    
    Co-authored-by: hoo00nn <oh.jihoon@linecorp.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   webpackFinal: async (config, { configType }) => {
     config.resolve.plugins = [new TsconfigPathsPlugin()];
@@ -11,6 +15,14 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-controls',
     '@storybook/addon-jest',
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+          modules: { localIdentName: '[name]__[local]--[hash:base64:5]' },
+        },
+      },
+    },
   ],
   typescript: {
     check: false,
